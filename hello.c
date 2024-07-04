7bf03968f9887f92cf4a38d0dd43217829ce8772
SASAKI Haruka <s1310206@u-aizu.ac.jp>
2024-07-04 14:37:50 +0900

    HoT: initial revision

---
+++ hello.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+
+int countH = 0;
+int countT = 0;
+
+printf("Tossing a coin...\n");
+
+srand( (int)time(NULL) );
+
+for(int i = 1; i < 4; i++){
+
+
+if(rand() % 2 == 0) {
+    printf("Round %d: Head\n",i);//Headは表
+    countH++;
+}
+
+else{
+printf("Round %d: Tail\n",i);
+countT++;
+}
+
+
+
+}
+
+printf("Heads %d, Tails %d\n",countH,countT);
+
+}
